0f540dddf446f0d757d2f49f913b66a673c82053
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-01-29 18:12:23 +0500

    New Div

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-alert("Ye phili dafa kaam kai hai ")
+// alert("Ye phili dafa kaam kai hai ")
 
-let elemenid = document.querySelector("p")
-console.dir(elemenid)
-let elemencls = document.getElementsByClassName("myhead")
-console.dir(elemencls)
+// let elemenid = document.querySelector("p")
+// console.dir(elemenid)
+// let elemencls = document.getElementsByClassName("myhead")
+// console.dir(elemencls)
 
-let head = document.querySelector("h1")
-head.innerText = head.innerText +  "saad"
+// let head = document.querySelector("h1")
+// head.innerText = head.innerText +  "saad"
+
+
+// let id = document.querySelector("p")
+// // console.log(id.getAttribute("my-para"))
+// console.log(id.setAttribute("class" , "Dangar"));
+let div = document.querySelector("div")
+console.log(div)
