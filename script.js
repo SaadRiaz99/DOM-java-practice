79c5ad67a43550d96f33c1b7bf31ea6cb1a0ff22
Saad Bin Riaz <malik.saad.riaz.96@gmail.com>
2026-01-29 17:46:54 +0500

    DOM practice

---
+++ script.js
@@ -0,0 +1,9 @@
+alert("Ye phili dafa kaam kai hai ")
+
+let elemenid = document.querySelector("p")
+console.dir(elemenid)
+let elemencls = document.getElementsByClassName("myhead")
+console.dir(elemencls)
+
+let head = document.querySelector("h1")
+head.innerText = head.innerText +  "saad"
